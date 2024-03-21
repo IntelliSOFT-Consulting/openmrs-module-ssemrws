845da3e2b1f0f34bb89ea721bb3703f1b2ea5491
Sharif Magembe <smagembe@idi.co.ug>
2024-03-21 12:04:17 +0300

    revert the initial changes

--- omod/src/test/java/org/openmrs/module/ssemrws/web/controller/SSEMRWebServicesControllerTest.java
+++ omod/src/test/java/org/openmrs/module/ssemrws/web/controller/SSEMRWebServicesControllerTest.java
@@ -4,22 +4,38 @@ import static org.junit.Assert.*;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.openmrs.Patient;
 import org.openmrs.api.context.Context;
 import org.openmrs.test.BaseModuleContextSensitiveTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Date;
 import java.util.List;
 
 @SuppressWarnings("deprecation")
+@RunWith(SpringJUnit4ClassRunner.class)
 public class SSEMRWebServicesControllerTest extends BaseModuleContextSensitiveTest {
-	
-	// private static final String DATASET_FILENAME =
-	// "org/openmrs/module/ssemrws/web/controller/resources/SSEMRWebServicesControllerTest.xml";
 	
 	@Before
 	public void setUp() throws Exception {
 		initializeInMemoryDatabase();
-		executeDataSet("org/openmrs/module/ssemrws/web/controller/resources/SSEMRWebServicesControllerTest.xml");
+		Patient patient = new Patient();
+		patient.setGender("M");
+		patient.setBirthdate(new Date());
+		patient.setBirthdateEstimated(false);
+		patient.setDead(false);
+		patient.setDeathDate(null);
+		patient.setCauseOfDeath(null);
+		patient.setCreator(Context.getAuthenticatedUser());
+		patient.setDateCreated(new Date());
+		patient.setChangedBy(null);
+		patient.setDateChanged(null);
+		patient.setVoided(false);
+		patient.setVoidedBy(null);
+		patient.setDateVoided(null);
+		patient.setVoidReason(null);
+		Context.getPatientService().savePatient(patient);
 	}
 	
 	@Test
