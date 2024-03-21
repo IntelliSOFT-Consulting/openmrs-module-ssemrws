b55b185a13602924c82544ee65bee113b0d93a3f
Sharif Magembe <smagembe@idi.co.ug>
2024-03-21 11:43:06 +0300

    initial commit test

--- omod/src/test/java/org/openmrs/module/ssemrws/web/controller/SSEMRWebServicesControllerTest.java
+++ omod/src/test/java/org/openmrs/module/ssemrws/web/controller/SSEMRWebServicesControllerTest.java
@@ -6,18 +6,20 @@ import org.junit.Before;
 import org.junit.Test;
 import org.openmrs.Patient;
 import org.openmrs.api.context.Context;
-import org.openmrs.test.jupiter.BaseModuleContextSensitiveTest;
+import org.openmrs.test.BaseModuleContextSensitiveTest;
 
 import java.util.List;
 
+@SuppressWarnings("deprecation")
 public class SSEMRWebServicesControllerTest extends BaseModuleContextSensitiveTest {
 	
-	private static final String DATASET_FILENAME = "SSEMRWebServicesControllerTest.xml";
+	// private static final String DATASET_FILENAME =
+	// "org/openmrs/module/ssemrws/web/controller/resources/SSEMRWebServicesControllerTest.xml";
 	
 	@Before
 	public void setUp() throws Exception {
-		executeDataSet(DATASET_FILENAME);
-		Context.getPatientService();
+		initializeInMemoryDatabase();
+		executeDataSet("org/openmrs/module/ssemrws/web/controller/resources/SSEMRWebServicesControllerTest.xml");
 	}
 	
 	@Test
